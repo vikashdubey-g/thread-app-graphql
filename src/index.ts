8f5e145d41be32f1f172b89ab5b128928d5473fe
Vikash Dubey <vikash.dubey@arshil.net>
2025-03-16 17:25:30 +0530

    code refactor

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,11 @@
-import express from "express";
-import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
-import { prismaClient } from "./lib/db";
+import express from "express";
+import createApolloServer from "./graphql";
 
 async function init() {
   const app = express();
   const PORT = Number(process.env.PORT) || 8000;
   app.use(express.json());
-
-  const gqlServer = new ApolloServer({
-    typeDefs: `
-      type Query {
-        hello: String
-        say(name: String): String
-      }
-      type Mutation {
-      createUser(firstName: String!, lastName: String!, email: String!, password: String!):Boolean      
-      }
-    `,
-    resolvers: {
-      Query: {
-        hello: () => `Hey there, I am a graphql server`,
-        say: (_, { name }: { name: string }) => `Hey ${name}, How are you?`,
-      },
-      Mutation: {
-        createUser: async (
-          _,
-          {
-            firstName,
-            lastName,
-            email,
-            password,
-          }: {
-            firstName: string;
-            lastName: string;
-            email: string;
-            password: string;
-          }
-        ) => {
-          await prismaClient.user.create({
-            data: {
-              email,
-              firstName,
-              lastName,
-              password,
-              salt: "random_dalt",
-              profileImageURL:"restaete"
-            },
-          });
-          return true;
-        },
-      },
-    },
-  });
-
-  await gqlServer.start();
 
   app.get("/", (req, res) => {
     res.json({ message: "Server is up and running" });
@@ -62,7 +13,7 @@ async function init() {
 
   app.use(
     "/graphql",
-    expressMiddleware(gqlServer, {
+    expressMiddleware(await createApolloServer(), {
       context: async ({ req }) => ({ req }),
     }) as unknown as express.RequestHandler
   );
